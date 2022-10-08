f42b0fd2e43b6ed04fa92b6bfd33392f093671a2
manish <Manishbisht9711@gmail.com>
2022-10-08 20:29:14 +0530

    list structure

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,59 +9,66 @@ const Footer = () => {
   return (
     <footer>
       <img src={logo} alt="" />
-      <ul>
+      <div className="foot-links">
         <p>Features</p>
-        <li>
-          <a aria-label="shortenlink" href="/">
-            Link Shortening
-          </a>
-        </li>
-        <li>
-          <a aria-label="branded-links" href="/">
-            Branded Links
-          </a>
-        </li>
-        <li>
-          <a aria-label="analytics" href="/">
-            Analytics
-          </a>
-        </li>
-      </ul>
-      <ul>
+        <ul>
+          <li>
+            <a aria-label="shortenlink" href="/">
+              Link Shortening
+            </a>
+          </li>
+          <li>
+            <a aria-label="branded-links" href="/">
+              Branded Links
+            </a>
+          </li>
+          <li>
+            <a aria-label="analytics" href="/">
+              Analytics
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="foot-links">
         <p>Resources</p>
-        <li>
-          <a href="/">Blog</a>
-        </li>
-        <li>
-          <a href="/">Developers</a>
-        </li>
-        <li>
-          <a href="/">Support</a>
-        </li>
-      </ul>
-      <ul>
+        <ul>
+          <li>
+            <a href="/">Blog</a>
+          </li>
+          <li>
+            <a href="/">Developers</a>
+          </li>
+          <li>
+            <a href="/">Support</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="foot-links">
         <p>company</p>
-        <li>
-          <a aria-label="about" href="/">
-            About
-          </a>
-        </li>
-        <li>
-          <a aria-label="out-team" href="/">
-            Our Team
-          </a>
-        </li>
-        <li>
-          <a aria-label="carrers" href="/">
-            Careers
-          </a>
-        </li>
-        <li>
-          <a aria-label="contact" href="/">
-            Contact
-          </a>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <a aria-label="about" href="/">
+              About
+            </a>
+          </li>
+          <li>
+            <a aria-label="out-team" href="/">
+              Our Team
+            </a>
+          </li>
+          <li>
+            <a aria-label="carrers" href="/">
+              Careers
+            </a>
+          </li>
+          <li>
+            <a aria-label="contact" href="/">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <div className="socials">
         <a aria-label="facebook-link" href="/">
